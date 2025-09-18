2745039168ccc8eb29b421775b760cb8cdd280d9
Gabriel Henrique de Oliveira Santos <39532888810@>
2025-09-18 11:27:08 -0300

    mexendo no front 1809

--- back/api/urls.py
+++ back/api/urls.py
@@ -1,7 +1,27 @@
 from django.urls import path
-from .views import AutoresView, listar_autores
+from .views import *
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
+
+    path('authors', listar_autores),
+
+    ###get e post
     path('autores', AutoresView.as_view()),
-    path('authors', listar_autores)
+    path('editoras', EditorasView.as_view()),
+    path('livros',LivrosView.as_view()),
+
+    ###update e delete
+    path('autor/<int:pk>', AutoresDetailView.as_view()),
+    path('editora/<int:pk>', EditorasDetailView.as_view()),
+    path('livro/<int:pk>', LivrosDetailView.as_view()),
+
+    ##token
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+ 
 ]
